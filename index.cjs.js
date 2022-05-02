c1f8f164efd527e752fa24cafe624090cf26d288
y12y17y <88134411+y12y17y@users.noreply.github.com>
2022-05-02 15:40:43 +0900

    Add files via upload

---
+++ index.cjs.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var vue = _interopDefault(require('vue'));
+
+//import movable from './components/movable.vue';
+// export default {
+// install(Vue) {
+// Vue.component("movable", movable);
+// }
+// };
